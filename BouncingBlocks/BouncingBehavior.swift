237963b7732461a4fdb62bb69176858a644d7230
Jagadish Uppala <uppala@Jagadishs-MacBook-Pro.local>
2016-03-04 22:58:29 -0800

    -Removed ViewController. +Added BouncingBehavior, GameSettings, BouncingBlocksVC to project

---
+++ BouncingBlocks/BouncingBehavior.swift
@@ -0,0 +1,13 @@
+//
+//  BouncingBehavior.swift
+//  BouncingBlocks
+//
+//  Created by Jagadish Uppala on 3/4/16.
+//  Copyright © 2016 Jagadish Uppala. All rights reserved.
+//
+
+import UIKit
+
+class BouncingBehavior: UIDynamicBehavior {
+
+}
---
+++ BouncingBlocks/GameSettings.swift
@@ -0,0 +1,9 @@
+//
+//  GameSettings.swift
+//  BouncingBlocks
+//
+//  Created by Jagadish Uppala on 3/4/16.
+//  Copyright © 2016 Jagadish Uppala. All rights reserved.
+//
+
+import Foundation
